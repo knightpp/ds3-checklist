e94cbe1922b7d36f0dc744edbc9876e51b164e4b
Danil Kondratiev <knightpp@protonmail.com>
2021-01-09 15:31:34 +0200

    remove old files

--- ds3_cheatsheet_parser/src/old/armor/types.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ArmorCategory {
-    pub(super) category: String,
-    pub(super) gear_names: Vec<String>,
-}
