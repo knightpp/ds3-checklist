c66ad21fae8c01526e760fb9e368889aa770200d
Danylo Kondratiev <knightpp@proton.me>
2024-07-21 13:41:13 +0300

    Use FHS

--- flake.nix
+++ flake.nix
@@ -29,39 +29,64 @@
     # Development environment output
     devShells =
       forAllSystems
-      ({pkgs}: {
-        default = let
-          androidComposition = pkgs.androidenv.composeAndroidPackages {
-            buildToolsVersions = ["34.0.0"];
-            platformVersions = ["34"];
-            abiVersions = ["armeabi-v7a" "arm64-v8a"];
-          };
-          androidSdk = androidComposition.androidsdk;
-          buildFlutter = pkgs.writeShellScriptBin "buildFlutter" ''
-            set -euo pipefail
-            export ANDROID_SDK_ROOT="${androidSdk}/libexec/android-sdk"
+      ({pkgs}: let
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          buildToolsVersions = ["34.0.0"];
+          platformVersions = ["34"];
+          abiVersions = ["armeabi-v7a" "arm64-v8a"];
+        };
+        androidSdk = androidComposition.androidsdk;
+        buildFlutter = pkgs.writeShellScriptBin "buildFlutter" ''
+          set -euo pipefail
+          export ANDROID_SDK_ROOT="${androidSdk}/libexec/android-sdk"
 
-            cd ds3_checklist
-            flutter pub get
-            flutter analyze --no-fatal-infos
-            flutter build "$@" --release
-          '';
-        in
-          pkgs.mkShell {
-            ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-
-            buildInputs = [
+          cd ds3_checklist
+          flutter pub get
+          flutter analyze --no-fatal-infos
+          flutter build "$@" --release
+        '';
+      in {
+        fhs = let
+          fhs = pkgs.buildFHSUserEnv {
+            name = "gradle-env";
+            targetPkgs = pkgs: [
               buildFlutter
               androidSdk
 
               pkgs.flutter
               pkgs.jdk
+              pkgs.gradle
+              pkgs.kotlin
               pkgs.flatbuffers
-              # it does not use system tools by default
-              # pkgs.gradle
-              # pkgs.aapt
+              pkgs.aapt
             ];
           };
+        in
+          pkgs.stdenv.mkDerivation {
+            name = "flutter-env-shell";
+            nativeBuildInputs = [fhs];
+
+            # this seems to trigger an infinite loop when direnv loads this nix file... how to avoid that?
+            shellHook = "exec gradle-env";
+
+            ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          };
+
+        default = pkgs.mkShell {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          # GRADLE_HOME = "${pkgs.gradle}";
+
+          buildInputs = [
+            buildFlutter
+            androidSdk
+
+            pkgs.flutter
+            pkgs.jdk
+            pkgs.flatbuffers
+            # pkgs.gradle
+            # pkgs.aapt
+          ];
+        };
       });
   };
 }
